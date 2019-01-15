5c840c52df4b4fde51e73bdf24809aad43b695b8
Ubuntu <Olamilekan@papercrawler.w5vxafhvtbkutf3c4dovliw4lf.bx.internal.cloudapp.net>
2019-01-15 14:00:18 +0000

    Azure play data

--- ayo/exceptions.py
+++
@@ -1,9 +0,0 @@
-class InvalidBoardAreaError(Exception):
-    """ Exception flagged when moves are attempted on an unknown area. """
-    pass
-
-
-class InvalidMoveError(Exception):
-    """ Exception flagged when no stones are available at given index. """
-    pass
-
